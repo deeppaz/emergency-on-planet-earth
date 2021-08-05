c33bf992e76482e57b112491c522e5dc4ad20ca9
jimmy durak <deeppaz@hotmail.com>
2021-08-05 18:28:40 +0300

    timer added and api file hidden by gitignore 04071b618bf1beb5cf6ea1bd514778ef8c6754ca

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const APIs = require('./apis');
 var T = new Tweeting({
     consumer_key: APIs.CONSUMER_KEY,
     consumer_secret: APIs.CONSUMER_SECRET,
-    access_token:APIs.ACCESS_TOKEN,
-    access_token_secret:APIs.ACCESS_SECRET_TOKEN,
-    timeout_ms: 60*1000,
+    access_token: APIs.ACCESS_TOKEN,
+    access_token_secret: APIs.ACCESS_SECRET_TOKEN,
+    timeout_ms: 60 * 1000,
     strictSSL: true
 });
 
-T.post('statuses/update', {status: 'test tweet3'},
-    function(err, data, response){
-        console.log(data);
-    }
-)
+async function TweetingFun() {
+    T.post('statuses/update', { status: 'test tweet5' },
+        function (err, data, response) {
+            console.log(data);
+        }
+    )
+}
+
+setInterval(TweetingFun, 2500);
